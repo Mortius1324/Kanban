2708dd5cd00cdc8fc77b1071f14535909e2bb79b
mortius1324 <mortius23@gmail.com>
2023-07-28 14:38:52 +0200

    Story 001: Adde first test controllers and some entities

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    #[Route('/')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    #[Route('/browse')]
+    public function browse(): Response
+    {
+        return new Response(
+            'Title: PB and JAMES'
+        );
+    }
+}
