e61e83ac4231f209155a19a6ac15053c539892a7
mortius1324 <mortius23@gmail.com>
2023-07-29 16:50:56 +0200

    Proba naprawy routingu

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/')]
 class LuckyController
 {
     #[Route('/')]
